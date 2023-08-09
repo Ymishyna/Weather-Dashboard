9116248c2dc84f06058bbb2d98223db61f9bc431
Yana Mishyna <yana.mishina.92@gmail.com>
2023-08-09 18:49:30 -0400

    created a function for getting coordinates through api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,28 @@ btnSearchEl.addEventListener('click', inputValidate)
 
 function inputValidate(e) {
  if(!searchInputEl.value){
+    alert("Please type the name of the city you are looking for");
     return;
  }
  e.preventDefault()
     var search = searchInputEl.value.trim();
  getCoordinates(search)
  searchInputEl.value = '';
+}
+
+//vaidated input of name of a city/location, next function is to get the geo coordinates api
+function getCoordinates(search) {
+    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid={API key}`
+
+    fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data[0])
+
+      getWeather(data[0])
+      }
+    );
 }
 
