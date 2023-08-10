3b978777cc81fcffcddee01f0e580003bb78bc29
Yana Mishyna <yana.mishina.92@gmail.com>
2023-08-10 17:57:47 -0400

    created a function for getting a weather and started a function shareData

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 //html variables
-var searchInputEl = document.getElementById("search")
-var btnSearchEl = document.getElementById("search-btn")
+var searchInputEl = document.getElementById("search");
+var btnSearchEl = document.getElementById("search-btn");
+var currentWeatherEl = document.getElementById("current-weather");
+var futureWeatherEl = document.getElementById("future-weather");
+
 
 //create an event listener for the button (search) --- "click", inputValidate
 btnSearchEl.addEventListener('click', inputValidate)
@@ -19,7 +22,7 @@ function inputValidate(e) {
 
 //vaidated input of name of a city/location, next function is to get the geo coordinates api
 function getCoordinates(search) {
-    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid={API key}`
+    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=82b45b8d50fb832bca393df388f7502c`
 
     fetch(apiURL)
     .then(function (response) {
@@ -33,3 +36,31 @@ function getCoordinates(search) {
     );
 }
 
+function getWeather(location) {
+
+  var {lat, lon } = location
+  
+  var city = location.name
+  
+  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=82b45b8d50fb832bca393df388f7502c`
+  
+  //pulling the weather 
+      fetch(apiURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+       //look at the data, determin what you need to solve - current and forecast
+       //call out each function , or I could use a function to share the info
+       shareData(data.list, city)
+      });
+      
+  }
+  
+  function shareData(weather, city) {
+      displayCurrent(weather[0])
+      displayForecast()
+      
+  }
+
+  
