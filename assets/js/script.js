9de8ff1a122b098e2d4998974ba42efafa4d673c
Yana Mishyna <yana.mishina.92@gmail.com>
2023-08-16 18:26:08 -0400

    fixed and orginezed better displayCurrent function, added some bootstrap styling and elements to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,102 +3,113 @@ var searchInputEl = document.getElementById("search");
 var btnSearchEl = document.getElementById("search-btn");
 var currentWeatherEl = document.getElementById("current-weather");
 var futureWeatherEl = document.getElementById("future-weather");
+var searchHistoryEl = document.getElementById("search-history");
 
 
-//create an event listener for the button (search) --- "click", inputValidate
-btnSearchEl.addEventListener('click', inputValidate)
-//function inputValidate -- purpose is to validate that there is a value in the input box
-
+// Function to validate input
 function inputValidate(e) {
- if(!searchInputEl.value){
+  e.preventDefault();
+
+  var searchValue = searchInputEl.value.trim();
+
+  if (searchValue === "") {
     alert("Please type the name of the city you are looking for");
     return;
- }
- e.preventDefault()
-    var search = searchInputEl.value.trim();
- getCoordinates(search)
- searchInputEl.value = '';
+  }
+
+  getCoordinates(searchValue);
+  searchInputEl.value = "";
 }
 
-//vaidated input of name of a city/location, next function is to get the geo coordinates api
+// Function to fetch coordinates of the city
 function getCoordinates(search) {
-    var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=82b45b8d50fb832bca393df388f7502c`
+  var apiURL = `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=82b45b8d50fb832bca393df388f7502c`;
 
-    fetch(apiURL)
+  fetch(apiURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(data[0])
-
-      getWeather(data[0])
+      if (data.length === 0) {
+        console.error("City not found.");
+        return;
       }
-    );
+      saveSearchHistory(data[0].name); // Save search history
+      getWeather(data[0]); // Move this line here
+      currentWeatherEl.classList.remove('d-none');
+      futureWeatherEl.classList.remove('d-none');
+    })
+    .catch(function (error) {
+
+    });
 }
 
+
+// Function to get and display weather data
 function getWeather(location) {
+  var { lat, lon } = location;
+  var city = location.name;
 
-  var {lat, lon } = location
-  
-  var city = location.name
-  
-  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=82b45b8d50fb832bca393df388f7502c&units=metric`
-  
-  //pulling the weather 
-      fetch(apiURL)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data); //pulling from line 52
-       //look at the data, determin what you need to solve - current and forecast
-       //call out each function , or I could use a function to share the info
-      //  shareData(data.list, city)
+  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=82b45b8d50fb832bca393df388f7502c&units=metric`;
+
+  fetch(apiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
       displayCurrent(data.list[0], city);
-      //calling funtion dataForecast. dataForecast(data.list)
-      });
-      
-  }
-  
-  function displayCurrent(weather, city) {
-    console.log(weather);
-    console.log(city);
-  
-    //I can start oulling my data
-    //creating vars for data
-    var temp = weather.main.temp;
-    var windspeed = weather.wind.speed;
-    var humid = weather.main.humidity;
-    // var iconDescr = weather.weather[0].description;
-    // var icon = weather.weather[0].icon; 
-    // console.log(temp, windspeed, humid, iconDescr, icon);
-    var currentContainer = document.getElementById('current-weather'); 
-    //we need city name, date, icon
-    //create a card to display
-    var card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    var cardBody = document.createElement('div');
-    cardBody.setAttribute('class', 'card-body'); //title and image
-    
+      displayForecast(prepareForecastData(data.list.slice(1)));
+    })
+    .catch(function (error) {
+      console.error("Error fetching weather data:", error);
+    });
+}
 
-    var heading = document.createElement('h5');
-    heading.setAttribute('class', 'card-title');
-    heading.textContent = city;
-    card.appendChild(heading);
-    card.append(cardBody);
+function displayCurrent(weather, city) {
+  var temp = weather.main.temp;
+  var windSpeed = weather.wind.speed;
+  var humidity = weather.main.humidity;
+  var currentDate = new Date().toLocaleDateString();
 
-    //display weather
-    var tempEl = document.createElement('p');
-    tempEl.setAttribute('class', 'card-text');
-    tempEl.textContent = `temp: ${temp}`;
+  var currentContainer = document.getElementById('current-weather');
+  currentContainer.innerHTML = '';
 
-    cardBody.appendChild(tempEl);
+  var card = document.createElement('div');
+  card.setAttribute('class', 'card mt-3');
+  var cardBody = document.createElement('div');
+  cardBody.setAttribute('class', 'card-body');
 
-    
-    currentContainer.append(card);
-  }
+  var heading = document.createElement('h4');
+  heading.setAttribute('class', 'card-title pt-2');
+  heading.style.fontWeight = 'bold '; // Apply inline style for bold text
+  heading.textContent = `${city} (${currentDate})`;
+  card.appendChild(heading);
+  card.append(cardBody);
 
 
+  var icon = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+  // Add weather icon
+  var iconImg = document.createElement('img');
+  iconImg.setAttribute('src', icon);
+  iconImg.setAttribute('alt', 'Weather Icon');
+  cardBody.appendChild(iconImg);
 
-  
+
+  var tempEl = document.createElement('p');
+  tempEl.setAttribute('class', 'card-text');
+  tempEl.textContent = `Temperature: ${temp} °C`;
+  cardBody.appendChild(tempEl);
+
+  var windEl = document.createElement('p');
+  windEl.setAttribute('class', 'card-text');
+  windEl.textContent = `Wind Speed: ${windSpeed} m/s`;
+  cardBody.appendChild(windEl);
+
+  var humidityEl = document.createElement('p');
+  humidityEl.setAttribute('class', 'card-text');
+  humidityEl.textContent = `Humidity: ${humidity}%`;
+  cardBody.appendChild(humidityEl);
+
+  currentContainer.appendChild(card);
+}
+
